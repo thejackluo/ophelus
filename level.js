40786e453338271dd4e6970b666ba3277ab2eafd
Jack Luo <jack145945@gmail.com>
2023-10-11 23:34:15 -0400

    create a level algorithm

---
+++ level.js
@@ -0,0 +1,7 @@
+const MAX_LEVEL = 100;
+const MIN_LEVEL = 1;
+
+// Level class: a way to track the level of a user
+/* 1. level: the level of the user from 1 - 1090 something that is \
+exponentially harder (100 levels is 250 hours of effort)
+*/
