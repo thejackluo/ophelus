db0626b09b247be7cf37658485ffee136bc32721
Jack Luo <jack145945@gmail.com>
2023-10-12 12:53:00 -0400

    AI ideas

---
+++ backend/elizabeth.js
@@ -0,0 +1,3 @@
+/*
+    Wouldn't it be cool if there is an assistant that can keep track of your reflection that you can talk to and give you insights on your productivity
+*/
---
+++ backend/ophelus.js
@@ -0,0 +1,3 @@
+/*
+    An LLM bot that determines your overall performance in projects based on task quality, estimated time, and other factors like proficiency 
+*/
