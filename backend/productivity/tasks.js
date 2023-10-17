ed2727d17b102f2587b497a6baa9174f5a784af3
Jack Luo <jack145945@gmail.com>
2023-10-16 23:52:14 -0400

    Update task with more unified object oriented design

--- backend/productivity/tasks.js
+++ backend/productivity/tasks.js
@@ -1,7 +1,8 @@
 /*
-    Task object: this is the most basic object that encaupsulates the task that the user will do. It is the most basic building block of the app.
-    The goal of this task object to make it as simple as possible but at the same time have enough functionality for most people
-    USER SET UP for TASK
+    Task object: this is the most basic object for Ophelus that encaupsulates the task that the user will do. 
+    It is the most basic building block of the app.
+    The goal of the task obejct design is to make it as simple as possible but at the same time provide enough functionality for most people.
+    ATTRIBUTES for TASK
         name of task
         description (optional)
         estimated duration of task (optional) (defaults to 15 minutes)
@@ -9,9 +10,13 @@
         priority for tasks (from 1 - 5 with default being 3) 
             (there is an important constraint here, you can only have 3 priority 5, 5 4s, and 12 3s) 
             (users can upgrade for more tasks as they level up or set future tasks into a bank)
-    PROFILE constraints
+    ATTRIBUTE CONSTRAINTS for TASK
         In total, a new user should be limited to 20 tasks, any other tasks shall be sent to storage (this is to prevent overloading the user)
         The user can upgrade their task limit by 5 every 5 levels for a maximum of 120 tasks
+    HELPER FUNCTION
+        autoPopulate: this is a function that automatically set up the indirect variables for this task such as the percentage completion and total time commitment 
+            (this is to make it easier for the user to see how much time they are committing to)
+
 */
 
 class Task {
@@ -28,7 +33,6 @@ class Task {
 
   // this is a function that automatically set up the indirect variables for this task such as the percentage completion and total time commitment
   autoPopulate() {
-    this.completed = 0; // a value between 0 and 100
-    this.totalTimeCommitment = this.duration; // a value in minutes
+    this.completed = 0; // a percentage value between 0 and 100
   }
 }
