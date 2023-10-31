57032b3b106ed12401e8204db65e62fb04f22c0d
Jack Luo <jack145945@gmail.com>
2023-10-31 03:48:01 -0400

    Main page tailwind test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+      <div className="bg-red-200">
+        <h1 className="text-blue-500">Hello World</h1>
+        <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+      </div>
     </main>
   );
 }
