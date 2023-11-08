9238f5e96940d00309b6762b9438849682f22b2d
Jack Luo <jack145945@gmail.com>
2023-11-07 19:17:33 -0500

    Uploaded background image for ophelus

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,12 @@ export default function Home() {
         <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
       </div>
       <div>
+        <Image
+          src="/city.webp"
+          width={1000}
+          height={760}
+          alt="A cyberpunk city"
+        />
         <h1 className={`${exo.className} antialiased`}>
           This should be a futuristic font
         </h1>
