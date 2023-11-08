3b24f823c44e85ea19d0ece727923c93626dc6d9
Jack Luo <jack145945@gmail.com>
2023-11-08 10:06:29 -0500

    Ophe Layout Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import { Young_Serif } from "next/font/google";
 import { exo } from "@/app/ui/fonts";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-red-200">
+      <div className="bg-red-100">
         <h1 className="text-blue-500">Hi World</h1>
-
+        <Link href="game">Press here</Link>
         {/* triangle */}
-        <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+        {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" /> */}
       </div>
       <div>
         <Image
