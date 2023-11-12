5fa5725502298c43a535f0b817ac94b9cd6d0790
Jack Luo <jack145945@gmail.com>
2023-11-12 15:32:34 -0500

    Temporarily remove types from next-auth-config

--- auth.config.ts
+++ auth.config.ts
@@ -1,1 +1,4 @@
-import type { NextAuthConfig } from "next-auth";
+// temporarily removed types from next auth config, will update later
+// import type { NextAuthConfig } from "next-auth";
+
+import { NextAuthConfig } from "next-auth";
