1719f3e6181dff8229559b242fc576b2adb59f06
Jack Luo <jack145945@gmail.com>
2023-11-13 15:00:35 -0500

    restructure: database -> API endpoint development

---
+++ backend/database/config.ts
@@ -0,0 +1,6 @@
+// This is where all the schema constant attributes are set up
+
+const WOOD = {
+  INIT_HP: 1000,
+  INIT_RES_COUNT: 15,
+};
---
+++ backend/database/db.ts
@@ -0,0 +1,13 @@
+// This is where most of prisma and endpoint functions are created for different categories
+
+// =======================
+// Gamification
+// =======================
+
+// =======================
+// Productivity
+// =======================
+
+// =======================
+// Social
+// =======================
