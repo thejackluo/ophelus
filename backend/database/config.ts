ed327323952efc1485632a6d79649d4067fea3b2
Jack Luo <jack145945@gmail.com>
2023-11-14 12:46:23 -0500

    added typescript functionality with config

--- backend/database/config.ts
+++ backend/database/config.ts
@@ -1,32 +1,39 @@
 // This is where all the schema constant attributes are set up
 
-// enum ResourceType {
-//   EMPTY
-//   WOOD
-//   GOLD
-//   MOUNTAIN
-//   ZOMBIE_HIVE
-// }
+enum ResourceType {
+  EMPTY,
+  WOOD,
+  GOLD,
+  MOUNTAIN,
+  ZOMBIE_HIVE,
+}
 
-// enum BuildingType {
-//   BASE
-//   WALL
-//   DOOR
-//   MARKET
-//   CONNECTOR
-//   GOLD_MINE
-//   GOLD_BOOSTER
-//   GOLD_STORAGE
-//   TREE_MINE
-//   TREE_BOOSTER
-//   TREE_STORAGE
-// }
+enum BuildingType {
+  BASE,
+  WALL,
+  DOOR,
+  MARKET,
+  CONNECTOR,
+  GOLD_MINE,
+  GOLD_BOOSTER,
+  GOLD_STORAGE,
+  TREE_MINE,
+  TREE_BOOSTER,
+  TREE_STORAGE,
+}
 
-const EMPTY = {
+type ResourceConfig = {
+  INIT_HP?: number;
+  MAX_RESOURCE_COUNT?: number;
+  RESOURCE_GEN?: boolean;
+  BUILDABLE: boolean;
+};
+
+const EMPTY: ResourceConfig = {
   BUILDABLE: true,
 };
 
-const WOOD = {
+const WOOD: ResourceConfig = {
   INIT_HP: 100,
   MAX_RESOURCE_COUNT: 50,
   RESOURCE_GEN: false,
