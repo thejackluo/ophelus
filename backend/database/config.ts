70eec9b6a375640682cb28ba8b82d526a4d198a1
Jack Luo <jack145945@gmail.com>
2023-11-14 12:23:55 -0500

    config: standardrize how json data is going to be stored

--- backend/database/config.ts
+++ backend/database/config.ts
@@ -1,8 +1,34 @@
 // This is where all the schema constant attributes are set up
 
-const WOOD = {
-  INIT_HP: 1000,
-  INIT_RES_COUNT: 15,
+// enum ResourceType {
+//   EMPTY
+//   WOOD
+//   GOLD
+//   MOUNTAIN
+//   ZOMBIE_HIVE
+// }
+
+// enum BuildingType {
+//   BASE
+//   WALL
+//   DOOR
+//   MARKET
+//   CONNECTOR
+//   GOLD_MINE
+//   GOLD_BOOSTER
+//   GOLD_STORAGE
+//   TREE_MINE
+//   TREE_BOOSTER
+//   TREE_STORAGE
+// }
+
+const EMPTY = {
+  BUILDABLE: true,
 };
 
-
+const WOOD = {
+  INIT_HP: 100,
+  MAX_RESOURCE_COUNT: 50,
+  RESOURCE_GEN: false,
+  BUILDABLE: false,
+};
