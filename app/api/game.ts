e40f13dc35c04d05599cae3de9b7b47500eda707
Jack Luo <jack145945@gmail.com>
2023-11-15 11:20:37 -0500

    initialize API endpoints

---
+++ app/api/game.ts
@@ -0,0 +1,11 @@
+/*
+    The API files is going be serving general purpose API endpoints (most likely RESTful) that are not related to any specific domain model.
+    However, the express server is most likely going to be serving the API and handles scaling
+    socket io is also potentially going to be used for real time communication
+*/
+
+/*
+    API endpoints:
+    - /game
+    - /game/:id (get, post, put, delete)
+*/
