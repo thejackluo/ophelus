e33990b36e2649ff2b527fa36c849e72e44ea1ce
Jack Luo <jack145945@gmail.com>
2023-11-15 11:36:04 -0500

    config.ts: update game database

--- backend/database/config.ts
+++ backend/database/config.ts
@@ -1,4 +1,6 @@
-// This is where all the schema constant attributes are set up
+// =======================
+// Schema Enums (structure)
+// =======================
 
 enum ResourceType {
   EMPTY,
@@ -22,8 +24,12 @@ enum BuildingType {
   TREE_STORAGE,
 }
 
+// =======================
+// Resourcces
+// =======================
+
 type ResourceConfig = {
-  INIT_HP?: number;
+  HP?: number;
   MAX_RESOURCE_COUNT?: number;
   RESOURCE_GEN?: boolean;
   BUILDABLE: boolean;
@@ -34,8 +40,101 @@ const EMPTY: ResourceConfig = {
 };
 
 const WOOD: ResourceConfig = {
-  INIT_HP: 100,
+  HP: 100,
   MAX_RESOURCE_COUNT: 50,
   RESOURCE_GEN: false,
   BUILDABLE: false,
+};
+
+const GOLD: ResourceConfig = {
+  HP: 500,
+  MAX_RESOURCE_COUNT: 250,
+  RESOURCE_GEN: true,
+  BUILDABLE: true,
+};
+
+const MOUNTAIN: ResourceConfig = {
+  HP: 5000,
+  MAX_RESOURCE_COUNT: 1000,
+  RESOURCE_GEN: true,
+  BUILDABLE: false,
+};
+
+const ZOMBIE_HIVE: ResourceConfig = {
+  HP: 10000,
+  MAX_RESOURCE_COUNT: 25,
+  RESOURCE_GEN: true,
+  BUILDABLE: false,
+};
+
+// =======================
+// Buildings
+// =======================
+
+type BuildingConfig = {
+  HP: number;
+  DEFENSE: number;
+  RESOURCE_BOOST?: number;
+  RESOURCE_CAPACITY?: number;
+};
+
+const BASE: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+};
+
+const WALL: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 100,
+};
+
+const DOOR: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 50,
+};
+
+const MARKET: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+};
+
+const CONNECTOR: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+};
+
+const GOLD_MINE: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+  RESOURCE_BOOST: 1,
+};
+
+const GOLD_BOOSTER: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+  RESOURCE_BOOST: 2,
+};
+
+const GOLD_STORAGE: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+  RESOURCE_CAPACITY: 1000,
+};
+
+const TREE_MINE: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+  RESOURCE_BOOST: 1,
+};
+
+const TREE_BOOSTER: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+  RESOURCE_BOOST: 2,
+};
+
+const TREE_STORAGE: BuildingConfig = {
+  HP: 1000,
+  DEFENSE: 0,
+  RESOURCE_CAPACITY: 1000,
 };
