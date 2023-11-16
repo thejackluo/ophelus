dd3d0ba2575b0463428a094bb1768628f55b3289
Jack Luo <jack145945@gmail.com>
2023-11-16 11:59:19 -0500

    frontend: updated page.tsx and determined function

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
 import { Young_Serif } from "next/font/google";
-import { exo } from "@/app/ui/fonts";
+import { exo } from "@/app/lib/fonts";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-red-100">
-        <h1>Hello everyone, welcome to Ophelus</h1>
-        <Link className="text-blue-500" href="game">
-          Press here
-        </Link>
-        {/* triangle */}
-        {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" /> */}
-      </div>
+      <h1>Hello everyone, welcome to Ophelus</h1>
+      <Link className="text-blue-500" href="game">
+        Press here
+      </Link>
       <div>
         <Image
           src="/city.webp"
@@ -22,8 +18,8 @@ export default function Home() {
           alt="A cyberpunk city"
         />
         <h1 className={`${exo.className} antialiased`}>
-          Here is a beautiful city generated using dalle 3, this font should
-          also be futuristic
+          This is a work in progress, but I hope that this project will grow to
+          be significant
         </h1>
       </div>
     </main>
