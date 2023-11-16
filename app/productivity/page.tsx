b8369cef4e77e62f0823480c225d9e4075657dae
Jack Luo <jack145945@gmail.com>
2023-11-16 12:09:04 -0500

    frontend: update productivity layout

--- app/productivity/page.tsx
+++ app/productivity/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Welcome to the productivity page, based on a page.tsx</h1>
-      <p>This is where all your tasks are tracked and went through :3</p>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Welcome to the productivity page! Based on page.tsx</h1>
+        <p>This is where all your tasks are tracked and went through :3</p>
+        <Link className="bg-blue-300" href={"../"}>
+          Back to home
+        </Link>
+      </div>
+    </main>
   );
 }
