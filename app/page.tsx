9d0f00f7cd44af74c034efe185a8a9fbb9d578a6
Jack Luo <jack145945@gmail.com>
2023-11-16 12:08:51 -0500

    Update main app layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,28 @@ import { exo } from "@/app/lib/fonts";
 import Image from "next/image";
 import Link from "next/link";
 
+// create a variable user that is a string
+const user = "Jack";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello everyone, welcome to Ophelus</h1>
-      <Link className="text-blue-500" href="game">
-        Press here
-      </Link>
-      <div>
+      {/* Hello World */}
+      <h1>Ophelus</h1>
+      {/* Nav Bar */}
+      <nav>
+        <Link className="text-blue-500 mx-2" href="game">
+          {user}&apos;s world
+        </Link>
+        <Link className="text-blue-500 mx-2" href="productivity">
+          {user}&apos;s work
+        </Link>
+        <Link className="text-blue-500 mx-2" href="social">
+          {user}&apos;s social
+        </Link>
+      </nav>
+      {/* Landing Page */}
+      <main>
         <Image
           src="/city.webp"
           width={1000}
@@ -18,10 +32,15 @@ export default function Home() {
           alt="A cyberpunk city"
         />
         <h1 className={`${exo.className} antialiased`}>
-          This is a work in progress, but I hope that this project will grow to
-          be significant
+          Ophelus is a work in progress, but I believe that this project will
+          yield to something significant.
         </h1>
-      </div>
+      </main>
+
+      {/* Footer */}
+      <footer>
+        <h1>Footer</h1>
+      </footer>
     </main>
   );
 }
