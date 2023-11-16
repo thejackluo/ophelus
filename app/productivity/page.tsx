9ddeaf9eaa5d27f5a82c8af8afe3574b45dd02e2
Jack Luo <jack145945@gmail.com>
2023-11-16 11:58:27 -0500

    frontend: refactored layout of folders

--- app/productivity/page.tsx
+++ app/productivity/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>Welcome to the productivity route, based on a page.tsx</h1>
+      <h1>Welcome to the productivity page, based on a page.tsx</h1>
       <p>This is where all your tasks are tracked and went through :3</p>
     </div>
   );
