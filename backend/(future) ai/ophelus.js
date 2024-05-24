f02c00c54cf2feb114258b221f5608782c91af5e
Jack Luo <jack145945@gmail.com>
2024-05-23 23:43:27 -0400

    begin of a new journey

--- backend/(future) ai/ophelus.js
+++ backend/(future) ai/ophelus.js
@@ -1,3 +1,8 @@
 /*
     An LLM bot that determines your overall performance in projects based on task quality, estimated time, and other factors like proficiency 
 */
+
+Class Ophelus {
+    constructor() {
+    }
+}
