8ccd6d44cab3a18353ecd3259544ebb0fe17e6ab
Jack Luo <jack145945@gmail.com>
2024-05-24 01:07:43 -0400

    Update Ophelus Description

--- backend/(future) ai/ophelus.js
+++ backend/(future) ai/ophelus.js
@@ -1,8 +1,15 @@
 /*
-    An LLM bot that determines your overall performance in projects based on task quality, estimated time, and other factors like proficiency 
+    Name: Ophelus
+    DescriptionAn LLM bot that determines your overall performance in projects based on task quality, estimated time, and other factors like proficiency 
+*/
+
+/*
+    For each category, have an object within that category
+
 */
 
 Class Ophelus {
     constructor() {
+
     }
 }
